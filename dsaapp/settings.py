10a8ec48c478bfdcba31ffccab494625baa5d867
Mineesha <mineesha6644@gmail.com>
2023-04-13 14:07:06 +0530

    Revert "Added templates"
    This reverts commit 45794600784b505fb59b91a1d350299a40430396.

--- dsaapp/settings.py
+++ dsaapp/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dsaapp',
 ]
 
 MIDDLEWARE = [
@@ -116,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- dsaapp/static/js/script.js
+++
@@ -1,7 +0,0 @@
-/* static/js/script.js */
-
-// Confirm attendance submission
-function confirmSubmit() {
-    return confirm('Are you sure you want to submit this attendance record?');
-  }
-  
