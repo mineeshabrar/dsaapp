167b581e6e48e921b3aa2199a9845d8ac36d42cf
lsharma-18 <83471814+lsharma-18@users.noreply.github.com>
2023-04-15 16:33:59 +0530

    Updated login home page (not final)

--- authentication/views.py
+++ authentication/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def login_view(request):
-    return render(request, 'base_login_page.html')
+    return render(request, 'login.html')
