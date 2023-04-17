f778e3817c60dd4ee758f335af135b7de68420f1
aabhaschopra <aabhaschopra@gmail.com>
2023-04-17 12:42:29 +0530

    Update settings.py

--- dsaapp/settings.py
+++ dsaapp/settings.py
@@ -3,11 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-z8x!m^*e3_1g&9yzm2p1q6_-@ykp&$7q(e1(pceh3$cc78a$y$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
