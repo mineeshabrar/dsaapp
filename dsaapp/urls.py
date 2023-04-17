07ba8662c18ac5823a39d3497f99b8559432603f
aabhaschopra <98276182+aabhaschopra@users.noreply.github.com>
2023-04-17 16:44:33 +0530

    Revert "Front end"

--- dsaapp/urls.py
+++ dsaapp/urls.py
@@ -1,14 +1,30 @@
+"""
+URL configuration for dsaapp project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
 from django.urls import path, include
 from student.views import *
 from heads.views import *
 from authentication.views import *
-#from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', login_view),
-    path('student/', student_view_data),  
-    path('', include('social_django.urls', namespace='social')),
-    path('logout/', logout_view),
+    path('login/student/', student_view_data), 
+    path('login/head/', head_view_data),
+    path('log_out', log_out),
+    path('login/', include('django.contrib.auth.urls')),
+    path('login/', include('authentication.urls')),
 ]
--- student/views.py
+++ student/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-
 
 def student_view_data(request):
     return render(request, 'nav_bar_student.html')
 
+def log_out(request):
+    return render(request, 'log_out.html')
