3d09a32063ed349c651969b208e37f7ba31aefa7
aabhaschopra <aabhaschopra@gmail.com>
2023-04-22 17:48:02 +0530

    Update my_adapter.py

--- authentication/my_adapter.py
+++ authentication/my_adapter.py
@@ -7,6 +7,7 @@ class MyAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         email_domain = sociallogin.user.email.split('@')[1].lower()
         if not email_domain == 'pec.edu.in':
-            raise ImmediateHttpResponse(HttpResponse(sociallogin.user.email + ' is not valid memeber of pec.edu.in'))
+            # raise ImmediateHttpResponse(HttpResponse(sociallogin.user.email + ' is not valid memeber of pec.edu.in'))
+            return render(request, 'login.html', {"error": sociallogin.user.email + ' is not valid memeber of pec.edu.in'})
         else:
             pass
