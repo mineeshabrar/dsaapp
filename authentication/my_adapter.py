ed908561aa175d8bd6533dafe8cb1044b01f6ee9
lsharma-18 <83471814+lsharma-18@users.noreply.github.com>
2023-04-22 21:35:34 +0530

    Removed http imports

--- authentication/my_adapter.py
+++ authentication/my_adapter.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
 from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
-from django.http import HttpResponse
-from allauth.exceptions import ImmediateHttpResponse
 from django.contrib import messages
 
 class MyAdapter(DefaultSocialAccountAdapter):
@@ -9,7 +7,6 @@ class MyAdapter(DefaultSocialAccountAdapter):
         email_domain = sociallogin.user.email.split('@')[1].lower()
         if not email_domain == 'pec.edu.in':
             messages.error(request, "Document deleted.")
-            # raise ImmediateHttpResponse(HttpResponse(sociallogin.user.email + ' is not valid memeber of pec.edu.in'))
             return render(request, 'login.html')
         else:
             pass
