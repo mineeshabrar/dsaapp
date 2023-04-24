824fea677182c71c7a8f001e35b23f68b290ab5b
ayushbhaimehta <liluzee>
2023-04-25 03:26:55 +0530

    updateStudentDash

--- student/views.py
+++ student/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def student_view_data(request):
-    return render(request, 'nav_bar_student.html')
+    return render(request, 'student_home_page.html')
 
 def log_out(request):
     return render(request, 'log_out.html')
