0f4fb8f3244fdb00ed962da56db35b8726931243
Annchit <63941503+Annchit@users.noreply.github.com>
2023-04-25 15:31:02 +0530

    Update db.py

--- student/db.py
+++ student/db.py
@@ -53,7 +53,6 @@ for student in students:
 #   "clubs": {
 #     "saasc": [
 #       {
-#         "event_id": "saasc2301",
 #         "name": "pecmun",
 #         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu",
 #         "event_organization": [
@@ -63,7 +62,88 @@ for student in students:
 #         "event_participation": [
 #           "19105123",
 #           "19105113"
-#         ]
+#         ],
+#         "event_id": "saasc2301"
+#       },
+#       {
+#         "name": "geoquiz",
+#         "description": "isegvkjaebjfdbciaebjviajbDKCfbaSKUFB",
+#         "event_organization": [
+#           "19105111",
+#           "19105123"
+#         ],
+#         "event_participation": [
+#           "19104011",
+#           "19105120"
+#         ],
+#         "event_id": "saasc2302"
+#       },
+#       {
+#         "name": "intrapec",
+#         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu",
+#         "event_organization": [
+#           "19105115",
+#           "19105118"
+#         ],
+#         "event_participation": [
+#           "19105123",
+#           "19105113"
+#         ],
+#         "event_id": "saasc2303"
+#       },
+#       {
+#         "name": "Cognition",
+#         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu",
+#         "event_organization": [
+#           "19105115",
+#           "19105118"
+#         ],
+#         "event_participation": [
+#           "19105123",
+#           "19105113"
+#         ],
+#         "event_id": "saasc2304"
+#       }
+#     ],
+#     "music": [
+#       {
+#         "name": "dastak",
+#         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu",
+#         "event_organization": [
+#           "19105115",
+#           "19105118"
+#         ],
+#         "event_participation": [
+#           "19105123",
+#           "19105113"
+#         ],
+#         "event_id": "music2301"
+#       },
+#       {
+#         "name": "aagaz",
+#         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu",
+#         "event_organization": [
+#           "19105115",
+#           "19105118"
+#         ],
+#         "event_participation": [
+#           "19105123",
+#           "19105113"
+#         ],
+#         "event_id": "music2302"
+#       },
+#       {
+#         "name": "chords",
+#         "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqu",
+#         "event_organization": [
+#           "19105115",
+#           "19105118"
+#         ],
+#         "event_participation": [
+#           "19105123",
+#           "19105113"
+#         ],
+#         "event_id": "music2303"
 #       }
 #     ]
 #   }
