df34d9a4f730f0a6ec77bc54157b8616aa7a3160
aabhaschopra <aabhaschopra@gmail.com>
2023-04-26 21:46:54 +0530

    if pec email not authenticated

--- authentication/my_adapter.py
+++ authentication/my_adapter.py
@@ -7,8 +7,8 @@ class MyAdapter(DefaultSocialAccountAdapter):
     def pre_social_login(self, request, sociallogin):
         email_domain = sociallogin.user.email.split('@')[1].lower()
         if not email_domain == 'pec.edu.in':
-            messages.error(request, "Document deleted.")
+            messages.error(request, "Please login with pec.edu.in")
             # return render(request, 'login.html')
-            raise ImmediateHttpResponse(redirect('/'))
+            raise ImmediateHttpResponse(redirect(''))
         else:
             pass
