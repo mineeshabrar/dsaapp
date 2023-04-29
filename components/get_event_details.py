60712f3f807921ccbb934bd7d7be23c489fd3ae3
lsharma-18 <83471814+lsharma-18@users.noreply.github.com>
2023-04-29 15:49:11 +0530

    sorted student events list

--- components/get_event_details.py
+++ components/get_event_details.py
@@ -1,2 +1,11 @@
+from conf import *
+
 def get_event_details(event_id):
-    return
+
+    collection_name = db["events"]
+    events = collection_name.find({})
+
+    for event in events:
+        if event["event_id"] == event_id:
+            print(event)
+            return event
--- student/views.py
+++ student/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.http import HttpResponseRedirect
+from datetime import datetime
 
 from components.conf import db
 
@@ -24,12 +25,18 @@ def student_final_view_data(request, sid):
                 for eventsOrg in s["events_organization"]:
                     event = collection_name.find_one({"event_id": eventsOrg})
                     eventsOrganized.append((event["date"] , event["name"]))
+                print(eventsOrganized)
+                eventsOrganized = sorted(eventsOrganized, key=lambda x:datetime.strptime(x[0], '%d-%m-%Y'))
+                print(eventsOrganized)
                 
 
             if len(s["events_participation"]) != 1:
                 for eventsPar in s["events_participation"]:
                     event = collection_name.find_one({"event_id": eventsPar})
                     eventsParticipated.append((event["date"], event["name"]))
+                print(eventsParticipated)
+                eventsParticipated = sorted(eventsParticipated, key=lambda x:datetime.strptime(x[0], '%d-%m-%Y'))
+                print(eventsParticipated)
                 
             return render(request, "student_landing_page.html", {"student": s, "eventsOrganized": eventsOrganized, "eventsParticipated": eventsParticipated})
 
