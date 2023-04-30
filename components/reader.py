d1ddb0da9911c33b060b2d717fb00af14e669fc5
aabhaschopra <aabhaschopra@gmail.com>
2023-04-30 14:49:09 +0530

    Update reader.py

--- components/reader.py
+++ components/reader.py
@@ -3,9 +3,16 @@ from bson import ObjectId
 from conf import *
 
 
-df = pd.read_excel("C:/Users/Aabhas/Downloads/3rd year prof.xlsx")
+df = pd.read_excel("C:/Users/Aabhas/Downloads/3rd year prof.xlsx", dtype=str)
+
+df["events_organization"] = df["events_organization"].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else [])
+df["events_participation"] = df["events_participation"].apply(lambda x: ast.literal_eval(x) if isinstance(x, str) else [])
+df["points"] = df["points"].fillna("")
+df["email"] = df["email"].fillna("")
+
 students = df.to_dict(orient="records")
 
 collection_name = db["students"]
-query = collection_name.update_many({"_id": ObjectId(ObjectIdStudents)}, {"$set": {"students": students}}, upsert=True)
-print("Documents updated:", query.modified_count)
+query = collection_name.insert_many(students)
+
+print("Documents updated:", len(query.inserted_ids))
