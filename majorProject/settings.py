420858a4f58923a12d041a44ab73dcba7b156d45
aabhaschopra <aabhaschopra@gmail.com>
2023-05-06 00:00:37 +0530

    Revert "collectstatic"
    This reverts commit aba56f74a905da68405d5273620f30d1003615d0.

--- majorProject/settings.py
+++ majorProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -156,12 +155,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if DEBUG:
-   STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'static'),
-   ]
-else:
-   STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
