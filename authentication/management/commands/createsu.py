98348c5de3b6ad416203b7641a3754849b18bdf9
aabhaschopra <aabhaschopra@gmail.com>
2023-05-06 01:23:56 +0530

    Update createsu.py

--- authentication/management/commands/createsu.py
+++ authentication/management/commands/createsu.py
@@ -3,12 +3,13 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Creates a superuser.'
-
     def handle(self, *args, **options):
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
                 username='admin',
                 password='admin'
             )
-        print('Superuser has been created.')
+            print('Superuser has been created.')
+        
+        else:
+            print('Superuser already exists.')
